74d5332f51e81e70903a7ea971bbd42b1ec0b4b2
patricpz <patricbarros19@gmail.com>
2026-03-26 08:01:58 -0300

    ajustes solucoes

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       <main>
         <Home />
         <WhatWeDo />
-        <Solutions />
+        {/* <Solutions /> */}
         <Projects />
         <Diferencial />
         <FinalCTA />
